da89ee5758226c39165c969601ad014791307e85
Parth <parthchauhan348@gmail.com>
2025-11-30 16:54:57 +0530

    Implement models, Controllers, utils

---
+++ Backend/src/routes/problemCreator.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const problemRouter =  express.Router();
+const adminMiddleware = require("../middleware/adminMiddleware");
+const {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem,solvedAllProblembyUser,submittedProblem, getProblemByIdForAdmin} = require("../controllers/userProblem");
+const userMiddleware = require("../middleware/userMiddleware");
+
+
+// Create
+problemRouter.post("/create",adminMiddleware ,createProblem);
+problemRouter.get("/getProblemById/:id",adminMiddleware, getProblemByIdForAdmin);
+problemRouter.put("/update/:id",adminMiddleware, updateProblem);
+problemRouter.delete("/delete/:id",adminMiddleware, deleteProblem);
+
+
+problemRouter.get("/problemById/:id",userMiddleware,getProblemById);
+problemRouter.get("/getAllProblem",userMiddleware, getAllProblem);
+problemRouter.get("/problemSolvedByUser",userMiddleware, solvedAllProblembyUser);
+problemRouter.get("/submittedProblem/:pid",userMiddleware,submittedProblem);
+
+
+module.exports = problemRouter;
+
+// fetch
+// update
+// delete 
